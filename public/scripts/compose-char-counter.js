b88a27f639d547ba25962300ee0f57059e553beb
Dante Cross <dantecross@outlook.com>
2020-11-18 01:14:27 +0000

    tweeter nav, heade, body. Text input field and counter function

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  let maxLength = 140;
+  $('textarea').keyup(function() {
+    let length = $(this).val().length;
+    let availableChar = maxLength - length;
+    $('.counter').text(availableChar);
+    if (availableChar < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', '#545149')
+    }
+  })
+  
+});
+
+// $(document).ready(function() {
+//   charAvailable = $('.counter').text(availableChar);
+//   if (charAvailable < 0) {
+//     $('.counter').css('color', 'red');
+//   }
+// })
